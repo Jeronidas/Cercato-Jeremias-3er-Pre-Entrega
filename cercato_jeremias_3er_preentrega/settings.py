2b2f9e69029881a5df34a63aa7104860fbbe0ce5
Jeremias <jerechercato@gmail.com>
2024-10-16 23:27:01 -0300

    'Agregue el primer template'

--- cercato_jeremias_3er_preentrega/settings.py
+++ cercato_jeremias_3er_preentrega/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'cercato_jeremias_3er_preentrega.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- inicio/views.py
+++ inicio/views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
+from django.template import Template, context, loader
+from datetime import datetime
+from django.shortcuts import render
 
 def inicio(request):
-    return HttpResponse('<h1> Soy la pantalla de inicio </h1>')
+    # return HttpResponse('<h1> Soy la pantalla de inicio </h1>')
+    return render(request, 'index.html')
